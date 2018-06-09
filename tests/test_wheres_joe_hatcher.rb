3248127cbdb2ae0654e176b7be0eca46bb7fc53d
Eponick <mikehuntwife@gmail.com>
2018-06-08 20:34:38 -0400

    Basic room class with testing

--- tests/test_wheres_joe_hatcher.rb
+++ tests/test_wheres_joe_hatcher.rb
@@ -3,8 +3,23 @@ require "test/unit"
 
 class Test_wheres_joe_hatcher < Test::Unit::TestCase
 
-  def test_sample
-    assert_equal(4, 2+2)
+  def test_room
+    home = Room.new("Home", "Exit to the north")
+    assert_equal(home.name, "Home")
+    assert_equal(home.desc, "Exit to the north")
+  end
+
+  def test_exits
+    home = Room.new("Home", "Exit to the north")
+    testroom = Room.new("Error", "Why are you here?")
+    north = Room.new("North room", "Hi there")
+    home.update_exits({'north' => north, 'test' => testroom})
+
+    assert_equal(home.get_exit("north"), north)
+    assert_equal(home.get_exit("test"), testroom)
+
+    try = home.get_exit("north")
+    assert_equal(try.name, "North room")
   end
 
 end
