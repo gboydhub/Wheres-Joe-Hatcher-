8a796f7c36eda66574a44eecb92ed99aa57f5578
Eponick <mikehuntwife@gmail.com>
2018-06-08 21:34:58 -0400

    Added alternte names to items

--- src/cItem.rb
+++ src/cItem.rb
@@ -4,6 +4,20 @@ class Item
         @name = name
         @desc = desc
         @usetext = usetext
+        @verbs = {}
+        @altnames = Array.new
+    end
+
+    def update_verbs(verbs)
+        @verbs.update(verbs)
+    end
+
+    def add_alt_name(name)
+        @altnames.push(name)
+    end
+
+    def is_called?(name)
+        name.eql? @altnames.detect{ |e| e == name}
     end
 
     attr_accessor     :name
--- tests/test_item_class.rb
+++ tests/test_item_class.rb
@@ -9,4 +9,11 @@ class Test_item_class < Test::Unit::TestCase
         assert_equal(obj.desc, "a shiny sword")
         assert_equal(obj.usetext, "With what?")
     end
+
+    def test_item_names
+        paper = Item.new("Paper", "scrap of paper", "It says: Hi")
+        paper.add_alt_name("scrap")
+        assert_true(paper.is_called?("scrap"))
+        assert_false(paper.is_called?("test"))
+    end
 end
