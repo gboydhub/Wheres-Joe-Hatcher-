a5cc5cf21191e2505565fc4ff51d62f5d6450d47
Eponick <mikehuntwife@gmail.com>
2018-06-09 01:15:24 -0400

    Building text parsing for input

--- src/wheres_joe_hatcher.rb
+++ src/wheres_joe_hatcher.rb
@@ -4,11 +4,50 @@ require_relative 'cItem'
 
 class Game
     def initialize()
+        @currentRoom = 0
         start()
     end
 
     def start()
-        puts "Hello"
+    end
+
+    def goto_room(room)
+        @currentRoom = room
+    end
+
+    def parse_command(input)
+        wordList = input.split
+        if wordList.length <= 0 then
+            return false
+        end
+
+        result = @currentRoom.try_verb(wordList[0])
+        if(!result || result.length == 1) then
+            return false
+        end
+
+        while(wordList.length > 2)
+            if ["is", "to", "a"].include?(wordList[1])
+                wordList[1] = wordList[2]
+                wordList.delete_at(2)
+            else
+                return false
+            end
+        end
+
+        case result
+        when "exit"
+            nextRoom = @currentRoom.get_exit(wordList[1])
+            if nextRoom then
+                goto_room(nextRoom)
+                return true
+            else
+                return false
+            end
+        else
+            return false
+        end
+        return false
     end
 end
 
--- tests/test_game_class.rb
+++ tests/test_game_class.rb
@@ -1,5 +1,25 @@
 require "./src/wheres_joe_hatcher.rb"
+require "./src/cRoom.rb"
 require "test/unit"
 
 class Test_game_class < Test::Unit::TestCase
+
+    def test_command
+
+        entrance = Room.new("Entrance", "You can go north.")
+        northroom = Room.new("The north", "So cold here")
+
+        entrance.update_exits({"north" => northroom})
+        entrance.update_verbs({"go" => "exit", "walk" => "exit"})
+        northroom.update_exits({"south" => entrance})
+        northroom.update_verbs({"go" => "exit", "walk" => "exit"})
+
+        tester = Game.new
+        tester.goto_room(entrance)
+        
+        assert_false(tester.parse_command("eat pizza"))
+        assert_true(tester.parse_command("walk north"))
+
+        assert_true(tester.parse_command("go to south"))
+    end
 end
