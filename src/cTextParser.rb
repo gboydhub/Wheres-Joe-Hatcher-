47de041a9eb343cbc7f7e3f93badfc0a64e89e15
Eponick <eponickstream@gmail.com>
2018-06-11 09:58:09 -0400

    Added basic class structure

---
+++ src/cTextParser.rb
@@ -0,0 +1,15 @@
+class TextParser
+    def initialize()
+        @wasValid = false
+        @parsedVerb = ""
+        @parsedSubject = ""
+    end
+
+    def parse_string(input)
+        
+    end
+
+    attr_reader     :wasValid
+    attr_reader     :parsedVerb
+    attr_reader     :parsedSubject
+end
