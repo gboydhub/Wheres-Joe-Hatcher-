1b9ceae1541bc7a96610c23f252adade745ec88f
Eponick <eponickstream@gmail.com>
2018-06-11 10:20:14 -0400

    Split strings into verb and subject, removing stops.

--- src/cTextParser.rb
+++ src/cTextParser.rb
@@ -13,7 +13,20 @@ class TextParser
             return
         end
 
-        
+        while(wordAry.length > 2)
+            if ["is", "to", "a", "the", "at"].include?(wordAry[1])
+                wordAry[1] = wordAry[2]
+                wordAry.delete_at(2)
+            else
+                wasValid = false
+                return
+            end
+        end
+
+        @parsedVerb = wordAry[0]
+        if wordAry.length > 1 then
+            @parsedSubject = wordAry[1]
+        end
 
         @wasValid = true
         return
--- tests/test_text_parser.rb
+++ tests/test_text_parser.rb
@@ -3,13 +3,18 @@ require "./src/cTextParser.rb"
 require "test/unit"
 
 class Test_text_parser < Test::Unit::TestCase
-    def test_has_string
+    def test_parser
         a = TextParser.new
         a.parse_string("hi there")
         assert_true(a.wasValid)
+        assert_equal(a.parsedVerb, "hi")
+        assert_equal(a.parsedSubject, "there")
+
         a.parse_string("")
         assert_false(a.wasValid)
+
         a.parse_string("test")
         assert_true(a.wasValid)
+        assert_equal(a.parsedVerb, "test")
     end
 end
